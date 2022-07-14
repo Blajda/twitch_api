9e4a210595b6fb8fbbc845bcbb41c5e7ddde4e79
David Blajda <db@davidblajda.com>
2022-07-13 21:15:37 -0400

    Create github workflow for integration tests

--- twitch_api/tests/common/mod.rs
+++ twitch_api/tests/common/mod.rs
@@ -4,20 +4,16 @@ use twitch_api::ClientConfig;
 
 use self::{
     mock_client::MockClient,
-    twitch_cli::{MockServer, MockServerBuilder},
 };
 
 pub mod mock_client;
-pub mod twitch_cli;
 
 pub struct TestContext {
-    pub server: MockServer,
     pub config: ClientConfig,
     pub mock_client: MockClient,
 }
 
 pub fn setup() -> Result<TestContext, Box<dyn Error>> {
-    let server = setup_mock_api()?;
     let config = ClientConfig {
         api_base_uri: "http://localhost:8080/mock".to_string(),
         auth_base_uri: "http://localhost:8080/auth".to_string(),
@@ -26,11 +22,6 @@ pub fn setup() -> Result<TestContext, Box<dyn Error>> {
 
     Ok(TestContext {
         mock_client: MockClient::build(),
-        server,
         config,
     })
-}
-
-pub fn setup_mock_api() -> std::io::Result<MockServer> {
-    MockServerBuilder::default().build()
 }
